dc11c5ffcb0002b096fc0f24f18da8b7088656c9
Nicholas Saunders <nksaunders1994@gmail.com>
2023-01-17 18:56:37 -1000

    updated track plot

--- emulator.py
+++ emulator.py
@@ -46,7 +46,7 @@ def bound_normal(mean=0, sd=1, low=0, upp=10, size=1):
                          loc=mean, size=size, scale=sd)
 
 
-def create_MESA_population(nstars=1, fk=7.5, rocrit=1.6, plot=True, save_dataframe=False,
+def create_MESA_population(nstars=10000, fk=7.5, rocrit=1.6, plot=True, save_dataframe=False,
                            df_path='MESA_sim_track.csv', include_uncertainties=False,
                            age_bounds=[5., 5., 0., 10.],
                            M_bounds=[1., .1, .8, 1.2],
@@ -75,7 +75,7 @@ def create_MESA_population(nstars=1, fk=7.5, rocrit=1.6, plot=True, save_datafra
 
     Teffs = 10**sim_y.T[0]
     Rs = 10**sim_y.T[1]
-    Zs = 10**sim_y.T[2]
+    surface_fehs = sim_y.T[2]
     Prots = 10**sim_y.T[3]
 
     Ls = (Rs**2) * ((Teffs / 5777)**4)
@@ -100,7 +100,7 @@ def create_MESA_population(nstars=1, fk=7.5, rocrit=1.6, plot=True, save_datafra
                                         fk_sample, rocrits,
                                         Teffs, err_Teff,
                                         Rs, err_R,
-                                        Zs, Prots, err_P]).T,
+                                        surface_fehs, Prots, err_P]).T,
                               columns=['Age', 'err_Age',
                                        'M', 'err_M',
                                        'L', 'err_L',
@@ -110,14 +110,14 @@ def create_MESA_population(nstars=1, fk=7.5, rocrit=1.6, plot=True, save_datafra
                                        'fk', 'rocrit',
                                        'Teff', 'err_Teff',
                                        'R', 'err_R',
-                                       'Z', 'P', 'err_P'])
+                                       'feh_surf', 'P', 'err_P'])
 
     else:
         sim_df = pd.DataFrame(np.array([age_sample, M_sample, Ls, feh_sample, Y_sample,
                                         alpha_sample, fk_sample, rocrit_sample,
-                                        Teffs, Rs, Zs, Prots]).T,
+                                        Teffs, Rs, surface_fehs, Prots]).T,
                                columns=['Age', 'M', 'L', 'feh', 'Yini', 'alpha',
-                                        'fk', 'rocrit', 'Teff', 'R', 'Z', 'P'])
+                                        'fk', 'rocrit', 'Teff', 'R', 'feh_surf', 'P'])
 
     if save_dataframe:
         sim_df.to_csv('MESA_sim_pop.csv', index=False)
@@ -128,7 +128,7 @@ def create_MESA_population(nstars=1, fk=7.5, rocrit=1.6, plot=True, save_datafra
     return sim_df
 
 
-def create_YREC_population(nstars=1, fk=7.5, rocrit=1.6, plot=True, save_dataframe=False,
+def create_YREC_population(nstars=10000, fk=7.5, rocrit=1.6, plot=True, save_dataframe=False,
                            df_path='YREC_sim_track.csv', include_uncertainties=False,
                            age_bounds=[5., 5., 0., 10.],
                            M_bounds=[1., .1, .8, 1.2],
@@ -154,7 +154,7 @@ def create_YREC_population(nstars=1, fk=7.5, rocrit=1.6, plot=True, save_datafra
 
     Teffs = 10**sim_y.T[0]
     Rs = 10**sim_y.T[1]
-    Zs = 10**sim_y.T[2]
+    surface_fehs = 10**sim_y.T[2]
     Prots = 10**sim_y.T[3]
 
     Ls = (Rs**2) * ((Teffs / 5777)**4)
@@ -177,7 +177,7 @@ def create_YREC_population(nstars=1, fk=7.5, rocrit=1.6, plot=True, save_datafra
                                         fk_sample, rocrits,
                                         Teffs, err_Teff,
                                         Rs, err_R,
-                                        Zs, Prots, err_P]).T,
+                                        surface_fehs, Prots, err_P]).T,
                               columns=['Age', 'err_Age',
                                        'M', 'err_M',
                                        'L', 'err_L',
@@ -186,14 +186,14 @@ def create_YREC_population(nstars=1, fk=7.5, rocrit=1.6, plot=True, save_datafra
                                        'fk', 'rocrit',
                                        'Teff', 'err_Teff',
                                        'R', 'err_R',
-                                       'Z', 'P', 'err_P'])
+                                       'feh_surf', 'P', 'err_P'])
 
     else:
         sim_df = pd.DataFrame(np.array([age_sample, M_sample, Ls, feh_sample,
                                         alpha_sample, fk_sample, rocrit_sample,
-                                        Teffs, Rs, Zs, Prots]).T,
+                                        Teffs, Rs, surface_fehs, Prots]).T,
                                columns=['Age', 'M', 'L', 'feh', 'alpha',
-                                        'fk', 'rocrit', 'Teff', 'R', 'Z', 'P'])
+                                        'fk', 'rocrit', 'Teff', 'R', 'feh_surf', 'P'])
 
     if save_dataframe:
         sim_df.to_csv('YREC_sim_pop.csv', index=False)
@@ -229,30 +229,22 @@ def create_MESA_track(npoints=10000, min_age=0.01, max_age=10., plot=True,
 
     Teffs = 10**sim_y.T[0]
     Rs = 10**sim_y.T[1]
-    Zs = 10**sim_y.T[2]
+    surface_fehs = 10**sim_y.T[2]
     Prots = 10**sim_y.T[3]
 
     Ls = (Rs**2) * ((Teffs / 5777)**4)
 
     sim_df = pd.DataFrame(np.array([age_sample, M_sample, Ls, feh_sample, Y_sample,
                                     alpha_sample, fk_sample, rocrit_sample,
-                                    Teffs, Rs, Zs, Prots]).T,
+                                    Teffs, Rs, surface_fehs, Prots]).T,
                            columns=['Age', 'M', 'L', 'feh', 'Yini', 'alpha',
-                                    'fk', 'rocrit', 'Teff', 'R', 'Z', 'P'])
+                                    'fk', 'rocrit', 'Teff', 'R', 'feh_surf', 'P'])
 
     if save_dataframe:
         sim_df.to_csv(df_path, index=False)
 
     if plot:
-        fig, ax = plt.subplots(1, 1, figsize=(5,3), dpi=150)
-
-        ax.plot(age_sample, Prots, c='cornflowerblue')
-        ax.set_xlabel('Age [Gyr]'); ax.set_ylabel('P [days]');
-
-        fig = plt.gcf()
-        fig.patch.set_facecolor('white')
-        fig.tight_layout()
-        plt.show()
+        plot_star(sim_df)
 
     return sim_df
 
@@ -280,32 +272,70 @@ def create_YREC_track(npoints=10000, min_age=0.01, max_age=10., plot=True,
 
     Teffs = 10**sim_y.T[0]
     Rs = 10**sim_y.T[1]
-    Zs = 10**sim_y.T[2]
+    surface_fehs = 10**sim_y.T[2]
     Prots = 10**sim_y.T[3]
 
     Ls = (Rs**2) * ((Teffs / 5777)**4)
 
     sim_df = pd.DataFrame(np.array([age_sample, M_sample, Ls, feh_sample,
                                     alpha_sample, fk_sample, rocrit_sample,
-                                    Teffs, Rs, Zs, Prots]).T,
+                                    Teffs, Rs, surface_fehs, Prots]).T,
                            columns=['Age', 'M', 'L', 'feh', 'alpha',
-                                    'fk', 'rocrit', 'Teff', 'R', 'Z', 'P'])
+                                    'fk', 'rocrit', 'Teff', 'R', 'feh_surf', 'P'])
 
     if save_dataframe:
         sim_df.to_csv(df_path, index=False)
 
     if plot:
-        fig, ax = plt.subplots(1, 1, figsize=(5,3), dpi=150)
-
-        ax.plot(age_sample, Prots, c='cornflowerblue')
-        ax.set_xlabel('Age [Gyr]'); ax.set_ylabel('P [days]');
-
-        fig = plt.gcf()
-        fig.patch.set_facecolor('white')
-        fig.tight_layout()
-        plt.show()
+        plot_star(sim_df)
 
     return sim_df
+
+
+def plot_star(df):
+
+    benchmarks = [1e-3, 1e-2, 1e-1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+    dims = (4,10)
+    plt.figure(dpi=120)
+    ax = plt.subplot2grid(dims, (0,0), colspan=4, rowspan=2)
+
+    ax.plot(df['Age'], df['P'], c='cornflowerblue')
+    ax.set_xlabel('Age [Gyr]'); ax.set_ylabel('P [days]');
+
+    ax = plt.subplot2grid(dims, (2,0), colspan=4, rowspan=2)
+
+    # plt.plot(df['Age'], df['L'], c='gold', label='L')
+    # plt.plot(df['Age'], df['R'], c='orangered', label='R')
+    # plt.plot(df['Age'], df['Teff']/5777, c='cornflowerblue', label=r'T$_{\rm eff}$')
+    ax.plot(df['Teff'], np.log10(df['P']), c='k', lw=1)
+    for i,b in enumerate(benchmarks):
+        if (b >= df['Age'].iloc[0]) and (b <= df['Age'].iloc[-1]):
+            ax.scatter(df['Teff'][np.argmin(np.abs(b-df['Age']))], np.log10(df['P'][np.argmin(np.abs(b-df['Age']))]),
+                        c=df['Age'][np.argmin(np.abs(b-df['Age']))], zorder=1000,
+                        vmin=df['Age'].iloc[0], vmax=df['Age'].iloc[-1], s=75)
+    ax.set_xlim(np.max(df['Teff'])+100, np.min(df['Teff'])-100)
+    ax.set_xlabel(r'T$_{\rm eff}$ [K]'); ax.set_ylabel('log(P) [days]');
+
+    ax = plt.subplot2grid(dims, (0,4), colspan=5, rowspan=4)
+    ax.plot(df['Teff'], np.log10(df['L']), c='k', lw=1)
+    for i,b in enumerate(benchmarks):
+        if (b >= df['Age'].iloc[0]) and (b <= df['Age'].iloc[-1]):
+            ax.scatter(df['Teff'][np.argmin(np.abs(b-df['Age']))], np.log10(df['L'][np.argmin(np.abs(b-df['Age']))]),
+                        c=df['Age'][np.argmin(np.abs(b-df['Age']))], zorder=1000,
+                        vmin=df['Age'].iloc[0], vmax=df['Age'].iloc[-1], s=75, label=f'{b} Gyr')
+    ax.legend(bbox_to_anchor=(1.0, 1.0), loc='upper left', fontsize=10)
+    # cax = ax.scatter(1, 1, c=1, vmin=df['Age'].iloc[0], vmax=df['Age'].iloc[-1])
+    # plt.colorbar(cax, label='Age [Gyr]')
+    ax.set_xlim(np.max(df['Teff'])+100, np.min(df['Teff'])-100)
+    ax.set_ylim(np.min(np.log10(df['L']))-.025, np.max(np.log10(df['L']))+.025)
+    ax.set_xlabel(r'T$_{\rm eff}$ [K]'); ax.set_ylabel(r'log(L) [L$_\odot$]');
+
+    fig = plt.gcf()
+    fig.patch.set_facecolor('white')
+    fig.set_size_inches([d+5 for d in dims[::-1]])
+    fig.tight_layout()
+    plt.show()
 
 
 def plot_sample(df, M_bounds=[1., .5, .8, 1.2]):
@@ -316,7 +346,7 @@ def plot_sample(df, M_bounds=[1., .5, .8, 1.2]):
     Teffs = np.array(df['Teff'])
     Ls = np.array(df['L'])
 
-    dims = (4,9)
+    dims = (4,10)
     plt.figure(dpi=120)
     ax = plt.subplot2grid(dims, (0,0), colspan=4, rowspan=2)
 
